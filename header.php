b31430ba14ef80b2e6e551cad2559d2b96ce3390
K <reecekol@gmail.com>
2014-09-14 17:15:15 -0400

    init

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+   <title>Barbados Attractions></title>
+   <link href='style.css' rel='stylesheet'> 
+</head>
+<nav class='main-nav'>
+  <ul> 
+    <li>things to do </li>
+	<li>places to go </li>
+	<li>Events and Festivals</li>
+    <li>travel safety</li>
+  </ul>
+</nav>
+<body>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'header.php'
+
+?>
+
+<div class="main">
+
+</div>
